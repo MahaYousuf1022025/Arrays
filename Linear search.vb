17c5208a85fc22daf6d21a6841b0fd93ef91fdc1
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2019-11-09 12:41:30 +0500

    Add files via upload

---
+++ Linear search.vb
@@ -0,0 +1,47 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim exammarks(6) As Integer
+        Dim index As Integer
+        Dim marks As Integer
+        Dim Isfound As Boolean
+        Dim x As Integer
+        Dim i As Integer
+
+        Console.WriteLine("enter your marks.")
+        For index = 1 To 6
+            marks = Console.ReadLine
+            exammarks(index) = marks
+
+        Next
+        Console.WriteLine(exammarks)
+
+        Isfound = False
+        Console.WriteLine("Enter the value you want to search")
+
+        x = Console.ReadLine
+        i = 0
+        While Isfound = False And i <= 5
+            i = i + 1
+            If exammarks(i) = x Then
+                Isfound = True
+            End If
+        End While
+        If Isfound = True Then
+            Console.WriteLine("Your value is found")
+        Else
+            Console.WriteLine("Your value is not found")
+        End If
+        Console.ReadKey()
+
+
+
+
+
+
+
+
+
+    End Sub
+
+End Module
