62c2a74c2de0f2584966c4acdacac6e0cb24392e
MahaYousuf1022025 <55849128+MahaYousuf1022025@users.noreply.github.com>
2019-11-18 07:18:42 +0500

    Add files via upload

---
+++ randomize.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim binstr As Integer
+        Dim total, count, Nextchar, power As Integer
+        binstr = Console.ReadLine
+        total = 0
+        For count = Len(binstr) To 1 Step -1
+            Nextchar = Mid(binstr, count)
+            power = Len(binstr) - count
+            total = total + (Val(Nextchar) * 2 ^ power)
+
+        Next
+
+        Console.WriteLine(total)
+        Console.ReadKey()
+
+
+
+
+
+
+
+    End Sub
+
+End Module
